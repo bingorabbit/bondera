1f5ddc60c04b454fc9fd49c54c9078c3cbd13314
Ibrahim Abdel Fattah Mohamed <ibrahim.abdelfattah@tedata.net>
2015-04-16 01:05:23 +0200

    Adds validation errors and messages

--- app/controllers.js
+++ app/controllers.js
@@ -1,5 +1,8 @@
 var bonderaControllers = angular.module('bonderaControllers', []);
 
 bonderaControllers.controller('MainCtrl', function($scope){
-
+    $scope.calculateFare = function(){
+        $scope.distanceError = ($scope.fareForm.$submitted || $scope.fareForm.distance.$touched) && $scope.fareForm.distance.$error.required
+        $scope.timeIntervalError = ($scope.fareForm.$submitted || $scope.fareForm.timeInterval.$touched) && $scope.fareForm.timeInterval.$error.required
+    }
 });
